ced221cc49e3e6847b606425a5e290c94642d48c
josiahparry <josiah.parry@gmail.com>
2021-03-14 18:57:47 -0400

    add data to plot inthe package

--- data-raw/data.R
+++ data-raw/data.R
@@ -9,4 +9,10 @@ available_pals <- list.files("data-raw/historic-pals", full.names = TRUE) %>%
   purrr::map(read_color_pal) %>% 
   purrr::flatten()
 
-usethis::use_data(available_pals, overwrite = TRUE)
+
+volcano_df <- as.data.frame(volcano) %>% 
+  mutate(id = row_number()) %>% 
+  tidyr::gather(y, z, -id) %>% 
+  mutate(y = as.numeric(str_remove(y, "V")))
+
+usethis::use_data(available_pals, volcano_df, overwrite = TRUE)
